2853c7319d30a80739c09a19a18badb775316a53
Richard Neumann <r.neumann@homeinfo.de>
2023-05-09 12:58:31 +0200

    Add deployments listing

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,5 @@
 <?php
 defined('TYPO3_MODE') || die();
+
+// allow table entries to be added to normal pages
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_deployments_domain_model_deployment');
